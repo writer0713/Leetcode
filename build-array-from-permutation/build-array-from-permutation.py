559616ca36ab2b4780aef272c0655e5ef551d7ff
writer0713 <writer0713@naver.com>
2023-04-30 00:55:22 +0900

    Time: 133 ms (6.77%) | Memory: 16.7 MB (36.11%) - LeetSync

--- build-array-from-permutation/build-array-from-permutation.py
+++ build-array-from-permutation/build-array-from-permutation.py
@@ -1,6 +1,3 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = []
-        for idx, num in enumerate(nums):
-            ans.insert(idx, nums[nums[idx]])
-        return ans
+        return [nums[num]for num in nums]
