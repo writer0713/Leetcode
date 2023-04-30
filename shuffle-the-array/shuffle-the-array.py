42221150b62be517e12052cd11983fe60303e045
writer0713 <writer0713@naver.com>
2023-04-30 13:26:32 +0900

    Time: 74 ms (5.78%) | Memory: 16.6 MB (5.40%) - LeetSync

--- shuffle-the-array/shuffle-the-array.py
+++ shuffle-the-array/shuffle-the-array.py
@@ -1,9 +1,6 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        arr1 = nums[:n]
-        arr2 = nums[n:]
         arr = []
-        for idx in range(n):
-            arr.append(arr1[idx])
-            arr.append(arr2[idx])
+        for i in range(n):
+            arr += [nums[i], nums[i + n]]
         return arr
