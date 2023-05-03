961c016c832232988f58c74b22643260d6f37aa1
writer0713 <writer0713@naver.com>
2023-05-03 22:02:51 +0900

    Time: 81 ms (12.54%) | Memory: 16.4 MB (6.04%) - LeetSync

--- palindrome-number/palindrome-number.py
+++ palindrome-number/palindrome-number.py
@@ -1,13 +1,5 @@
-from collections import deque
-
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        dq = deque(list(str(x)))
-        while len(dq) > 1:
-            left = dq.popleft()
-            right = dq.pop()
-            if left != right:
-                return False
-        
-        return True
+        target = str(x)
+        return target == target[::-1]
         
