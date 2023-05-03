a7b9345b246170f61b75a1332e36f02400132170
writer0713 <writer0713@naver.com>
2023-05-03 22:44:20 +0900

    Time: 59 ms (20.10%) | Memory: 16.3 MB (8.02%) - LeetSync

--- roman-to-integer/roman-to-integer.py
+++ roman-to-integer/roman-to-integer.py
@@ -1,22 +1,19 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
+    def romanToInt(self, romans: str) -> int:
 
-        m = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+        romanIntegers = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
         result = 0
         
-        for i in range(len(s)):
-            current = m[s[i]]
+        for idx in range(1, len(romans)):
+            first = romanIntegers[romans[idx - 1]]
+            second = romanIntegers[romans[idx]]
 
-            if i == len(s) - 1:
-                result += current
-                break
-
-            next = m[s[i + 1]]
-
-            if (current < next):
-                result -= current
+            if (first < second):
+                result -= first
             else:
-                result += current
+                result += first
+
+        result += romanIntegers[romans[-1]]
         
         return result
             
