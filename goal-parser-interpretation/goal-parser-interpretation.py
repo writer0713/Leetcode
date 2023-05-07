61b3736924cf8ee3f7c3a2bd352d14c306261dd9
writer0713 <writer0713@naver.com>
2023-05-07 15:54:33 +0900

    Time: 48 ms (5.92%) | Memory: 16.2 MB (6.76%) - LeetSync

--- goal-parser-interpretation/goal-parser-interpretation.py
+++ goal-parser-interpretation/goal-parser-interpretation.py
@@ -1,5 +1,25 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        return command.replace("()", "o").replace("(al)", "al")
+
+        idx = 0
+        result = ""
+
+        while idx <= len(command) - 1:
+
+            if command[idx] == "G":
+                result += "G"
+            elif command[idx] == "(" and command[idx + 1] == ")":
+                result += "o"
+                idx += 1
+            else: # (al)
+                result += "al"
+                idx += 3
+
+            idx += 1
+
+        return result
+
+
+
 
         
